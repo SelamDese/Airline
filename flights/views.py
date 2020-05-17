fbe901c0efbe0078dedc4961a98bbb36b7d6838a
millket <milliyon.ketera@gmail.com>
2020-05-16 22:38:04 -0500

    /id route

--- flights/views.py
+++ flights/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.shortcuts import render
 from .models import Flight
 
@@ -6,6 +6,15 @@ from .models import Flight
 def index(request):
     # return HttpResponse("flights")
     context = {
-        "flights" : Flight.objects.all()
+        "flights" : Flights.objects.all()
     }
     return render(request, "flights/index.html", context)
+ def flight(request, flight_id):
+     try:
+     flight = Flight.objects.get(pk=flight_id)
+     except Flight.DoesNotExist:
+             raise Http404("Flight does not exist")
+    context = {
+        "flight": flight
+     }
+     return render(request, "flights/flight.html", context)
